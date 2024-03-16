54ef1068327cd64c00e74e5293a0e5916bea0387
Andres Santamarina <andresesantamarina@gmail.com>
2024-03-16 01:27:44 -0300

    agregue la logica para editar una receta

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -35,3 +35,22 @@ export const crearReceta = async (req, res)=>{
         })
     }
 }
+
+export const editarReceta = async (req,res)=>{
+    try {
+        const buscarReceta = await Receta.findById(req.params.id)
+        if(!buscarReceta){
+            return res.status(404).json({
+                mensaje: "No se puedo editar la receta, el id es incorrecto"
+            })
+        }
+        await Receta.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({
+            mensaje: "La receta fue modificada correctamente"
+        })
+    } catch (error) {
+        res.status(500).json({
+            mensaje: "Ocurrió un error al intentar editar la receta"
+        })
+    }
+}
--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { crearReceta, listarRecetas, obtenerReceta } from "../controllers/recetas.controllers.js";
+import { crearReceta, editarReceta, listarRecetas, obtenerReceta } from "../controllers/recetas.controllers.js";
 
 const router = Router()
 
 router.route('/recetas').get(listarRecetas).post(crearReceta)
-router.route('/receta/:id').get(obtenerReceta)
+router.route('/receta/:id').get(obtenerReceta).put(editarReceta)
 
 export default router;
